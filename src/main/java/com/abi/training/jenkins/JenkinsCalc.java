a53e1536d7f8f87d328f1095de17915be2f90adb
AbiramiKarunanithi <abiramikarunanithi@gmail.com>
2021-08-11 20:23:14 +0530

    Adding working dir

---
+++ src/main/java/com/abi/training/jenkins/JenkinsCalc.java
@@ -0,0 +1,25 @@
+package com.abi.training.jenkins;
+
+public class JenkinsCalc {
+	
+	//Method to ADD 2 numbers
+	public int addNumbers(int numberOne, int numberTwo) {
+		return numberOne + numberTwo;
+	}
+	
+	//Method to SUBTRACT 2 numbers
+	public int subtractNumbers(int numberOne, int numberTwo) {
+		return numberOne - numberTwo;
+	}
+
+	//Method to Multiply 2 numbers
+	public int multiplyNumbers(int numberOne, int numberTwo) {
+		return numberOne * numberTwo;
+	}
+
+	//Method to Divide 2 numbers
+	public int divideNumbers(int numberOne, int numberTwo) {
+		return numberOne / numberTwo;
+	}
+	
+}
